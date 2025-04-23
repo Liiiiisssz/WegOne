4b583abf99d17247991b5eb04b92fff339390501
Liiiiisssz <elis_jasper@estudante.sesisenai.org.br>
2025-04-23 19:10:47 -0300

    Chore: Desenvolvimento inicial da TelaInicial

--- TelaInicial.java
+++ TelaInicial.java
@@ -11,6 +11,51 @@ public class TelaInicial {
     public static void main(String[] args) {
 
         Scanner leia = new Scanner(System.in);
+       
+        int decisaoInicial;
+       
+        System.out.println("Bem vindo ao Sistema de Cadastro de Orientações WEG!");
 
+        System.out.println(" ");
+       
+        System.out.println("  1.  Cadastrar Orientação ");
+        System.out.println("  2.  Pesquisar Orientação ");
+        System.out.println("  3.  Editar Orientação    ");
+        System.out.println("  4.  Excluir Orientação   ");
+        System.out.println("  5.  ENCERRAR PROGRAMA    ");
+       
+        System.out.println(" ");
+
+
+        System.out.println("O que deseja realizar");
+        decisaoInicial = leia.nextInt();
+       
+        System.out.println(" ");
+       
+        switch(decisaoInicial) {
+       
+        case 1:
+            System.out.println("CADASTRAR ORIENTAÇÃO");
+
+
+            break;
+       
+        case 2:
+            System.out.println("Pesquisar Orientação");
+
+            break;
+           
+        case 3:
+            System.out.println("Editar Orientação");
+
+
+            break;
+           
+        case 4:
+            System.out.println("Excluir Orientação");
+
+
+            break;
+        }
     }
 }
