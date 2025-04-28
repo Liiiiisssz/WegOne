20c5d41fcaa64ae0be0ba6dd9c10bc37e44cb689
Elis Jasper <elis_jasper@estudante.sesisenai.org.br>
2025-04-28 16:49:11 -0300

    Feat: adição do método cadastro

--- ManualCondutaOperacoesSetoriais.java
+++ ManualCondutaOperacoesSetoriais.java
@@ -1,3 +1,52 @@
+import java.util.Scanner;
+
 public class ManualCondutaOperacoesSetoriais {
+
+    Scanner leia = new Scanner(System.in);
+
+    String[] manuaisPadrao = new String[2];
+	String[] titulosManuaisPadrao = new String[2];
+
+	String[] manuaisCadastro = new String[2];
+	String[] titulosManuaisCadastro = new String[2];
+
+    int contadorCadastro = 0;
+
+    public ManualCondutaOperacoesSetoriais(){
+
+        titulosManuaisPadrao[0] = " ";
+
+        manuaisPadrao[0] = " ";
+
+        titulosManuaisPadrao[1] = " ";
+
+        manuaisPadrao[1] = " ";
+
+    }
+
+    public void cadastrarManual(){
+
+        if (contadorCadastro < 2) {
+
+			System.out.println("Titulo do Manual de Operação: ");
+			titulosManuaisCadastro[contadorCadastro] = leia.nextLine();
+
+			System.out.println(" ");
+
+			System.out.println("Conteúdo do Manual de Operação: ");
+			manuaisCadastro[contadorCadastro] = leia.nextLine();
+			System.out.println(" ");
+
+			contadorCadastro++;
+
+		} else {
+
+			System.out.println("Número máximo de manuais atingido!");
+
+		}
+
+    }
+
+
     
 }
--- TelaInicial.java
+++ TelaInicial.java
@@ -2,17 +2,15 @@ import java.util.Scanner;
 
 public class TelaInicial {
 
-    public static String[] Orientacoes_titulos = new String[20];
-    public static String[] Orientacoes_tipos = { "Manual Operações", "Procedimento de Segurança",
-            "Manutenção e Reparos",
-            "Testes e Diagnósticos", "Manual de Conduta e Operações Setoriais" };
-
     public static void main(String[] args) {
 
         Scanner leia = new Scanner(System.in);
 
         ManualOperacao manualOperacao = new ManualOperacao();
         ManualOperacao manualOpCadastro = new ManualOperacao();
+
+        ManualCondutaOperacoesSetoriais manualCondutaOpSet = new ManualCondutaOperacoesSetoriais();
+        ManualCondutaOperacoesSetoriais manualCondutaOpSetCadastro = new ManualCondutaOperacoesSetoriais();
 
         int decisaoInicial;
         int tipoOrientacao;
