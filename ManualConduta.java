1939820ac400fddd32eb6703b942b6301ae421c6
Liiiiisssz <elis_jasper@estudante.sesisenai.org.br>
2025-04-28 21:47:10 -0300

    Fix: correção do array da classe ManualConduta

--- ManualConduta.java
+++ ManualConduta.java
@@ -4,57 +4,18 @@ public class ManualConduta {
 
     Scanner leia = new Scanner(System.in);
 
-    String[] manuaisConduta = new String[2];
-    String[] titulosManuaisConduta = new String[2];
-
-    String[] manuaisCadastroConduta = new String[2];
-    String[] titulosManuaisCadastroConduta = new String[2];
+    String[] manualCondutaConteudo = new String[4];
+    String[] titulosManuaisConduta = new String[4];
 
     public ManualConduta() {
 
         titulosManuaisConduta[0] = "bla bla";
 
-        manuaisConduta[0] = "bla bla";
+        manualCondutaConteudo[0] = "bla bla";
 
-        titulosManuaisConduta[0] = "bla bla";
+        titulosManuaisConduta[1] = "bla bla";
 
-        manuaisConduta[0] = "bla bla";
-
-    }
-
-    public void cadastrarManual() {
-
-        int contador = 0;
-
-        	if (contador < 2) {
-        		
-                 System.out.print("Titulo do Manual de Conduta: ");
-                 titulosManuaisCadastroConduta[contador] = leia.nextLine();
-                 
-                 System.out.println(" ");
-
-                 System.out.println("Conteúdo do Manual de Conduta: ");
-                 manuaisCadastroConduta[contador] = leia.nextLine();
-                 System.out.println(" ");
-
-                 contador++;
-                 
-            }
-
-    }
-
-    public void pesquisarManual() {
-
-        System.out.println("--Pesquisar um manual--");
-        System.out.println("");
-
-    }
-
-    public void editarManual() {
-
-    }
-
-    public void excluirManual() {
+        manualCondutaConteudo[1] = "bla bla";
 
     }
 
