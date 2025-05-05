772c8cea0f38a87f517e60eac64b6e3dd6b27b4f
melll1601 <melissa_r_pereira@estudante.sesisenai.org.br>
2025-05-05 19:56:09 -0300

    Style: Arrumando manualCOS [1].

--- traducao/src/main/java/com/ManualCondutaOperacoesSetoriais.java
+++ traducao/src/main/java/com/ManualCondutaOperacoesSetoriais.java
@@ -110,103 +110,103 @@ public class ManualCondutaOperacoesSetoriais {
         titulosManuaisCOS[1] = "Procedimentos Operacionais no Setor de Manutencao";
 
         manualCOSConteudo[1] =
-                "_______________________________________________________________________\n" +
-                "|-- OBJETIVO: Estabelecer os procedimentos operacionais padrão para    --|\n" +
-                "|    o setor de manutenção da WEG, garantindo a eficiência e a segurança --|\n" +
-                "|_____________________________________________________________________|\n" +
-                "|                                                                     |\n" +
-                "| 1. OBJETIVO DO SETOR DE MANUTENÇÃO:                                 |\n" +
-                "|                                                                     |\n" +
-                "| - Garantir a operação contínua e segura dos equipamentos e sistemas |\n" +
-                "|   da WEG.                                                           |\n" +
-                "| - Realizar manutenção preventiva, corretiva e preditiva de acordo com |\n" +
-                "|   os planos e cronogramas estabelecidos.                             |\n" +
-                "|---------------------------------------------------------------------|\n" +
-                "|                                                                     |\n" +
-                "| 2. MANUTENÇÃO PREVENTIVA:                                           |\n" +
-                "|                                                                     |\n" +
-                "| - Realizar inspeções regulares em todos os equipamentos e sistemas.  |\n" +
-                "| - Seguir os cronogramas de manutenção preventiva, conforme o manual  |\n" +
-                "|   do fabricante.                                                    |\n" +
-                "| - Lubrificação dos componentes conforme especificado no manual de   |\n" +
-                "|   operação e manutenção.                                            |\n" +
-                "| - Verificar e substituir filtros, correias, rolamentos, etc., conforme|\n" +
-                "|   necessário, para evitar falhas futuras.                           |\n" +
-                "|---------------------------------------------------------------------|\n" +
-                "|                                                                     |\n" +
-                "| 3. MANUTENÇÃO CORRETIVA:                                            |\n" +
-                "|                                                                     |\n" +
-                "| - Quando ocorrer falha ou defeito em algum equipamento, o setor de  |\n" +
-                "|   manutenção deverá realizar a análise do problema, elaborar um      |\n" +
-                "|   plano de reparo e corrigir a falha com a maior rapidez possível.  |\n" +
-                "| - Após a correção, realizar testes para garantir o funcionamento     |\n" +
-                "|   correto do equipamento.                                           |\n" +
-                "| - Documentar o defeito e o procedimento de reparo, para futuras      |\n" +
-                "|   referências e controle.                                           |\n" +
-                "|---------------------------------------------------------------------|\n" +
-                "|                                                                     |\n" +
-                "| 4. MANUTENÇÃO PREDITIVA:                                            |\n" +
-                "|                                                                     |\n" +
-                "| - Utilizar técnicas de monitoramento de condição (como termografia, |\n" +
-                "|   vibração e ultrassom) para identificar potenciais falhas antes    |\n" +
-                "|   que ocorram.                                                      |\n" +
-                "| - Implementar tecnologias de análise preditiva para otimizar as     |\n" +
-                "|   intervenções de manutenção e reduzir os tempos de inatividade.    |\n" +
-                "| - Manter registros detalhados dos dados coletados para análise e    |\n" +
-                "|   previsão de falhas futuras.                                       |\n" +
-                "|---------------------------------------------------------------------|\n" +
-                "|                                                                     |\n" +
-                "| 5. PROCEDIMENTOS DE SEGURANÇA NA MANUTENÇÃO:                        |\n" +
-                "|                                                                     |\n" +
-                "| - Desligar e isolar as fontes de energia antes de realizar qualquer |\n" +
-                "|   intervenção em equipamentos.                                      |\n" +
-                "| - Utilizar Equipamentos de Proteção Individual (EPIs) adequados para|\n" +
-                "|   cada tipo de tarefa, como luvas, óculos, capacetes, protetores    |\n" +
-                "|   auditivos e calçados de segurança.                                |\n" +
-                "| - Assegurar que todas as ferramentas e dispositivos utilizados estejam|\n" +
-                "|   em boas condições de uso e seguiam as normas de segurança.        |\n" +
-                "| - Em caso de necessidade de intervenções em áreas de risco, seguir  |\n" +
-                "|   as normas específicas de segurança para o trabalho em altura, em  |\n" +
-                "|   espaços confinados ou com risco elétrico.                         |\n" +
-                "|---------------------------------------------------------------------|\n" +
-                "|                                                                     |\n" +
-                "| 6. DOCUMENTAÇÃO E REGISTROS DE MANUTENÇÃO:                          |\n" +
-                "|                                                                     |\n" +
-                "| - Manter um registro atualizado de todas as atividades de manutenção,|\n" +
-                "|   incluindo a data, descrição do serviço realizado e peças trocadas.|\n" +
-                "| - Os registros devem ser armazenados de forma organizada e de fácil |\n" +
-                "|   acesso, para futuras auditorias ou referência.                    |\n" +
-                "| - Elaborar relatórios de falhas, identificando a causa raiz e as    |\n" +
-                "|   ações corretivas aplicadas.                                       |\n" +
-                "|---------------------------------------------------------------------|\n" +
-                "|                                                                     |\n" +
-                "| 7. TREINAMENTO E CAPACITAÇÃO:                                       |\n" +
-                "|                                                                     |\n" +
-                "| - Todos os colaboradores do setor de manutenção devem participar de |\n" +
-                "|   treinamentos periódicos sobre novas tecnologias, procedimentos e  |\n" +
-                "|   normas de segurança.                                              |\n" +
-                "| - A WEG oferece programas de treinamento contínuo para garantir que |\n" +
-                "|   os colaboradores estejam sempre atualizados com as melhores       |\n" +
-                "|   práticas de manutenção e segurança.                               |\n" +
-                "|---------------------------------------------------------------------|\n" +
-                "|                                                                     |\n" +
-                "| 8. PROCEDIMENTOS EM CASO DE INCIDENTES OU ACIDENTES:                |\n" +
-                "|                                                                     |\n" +
-                "| - Caso ocorra qualquer tipo de acidente durante a manutenção, o     |\n" +
-                "|   colaborador deverá imediatamente acionar o supervisor e a equipe  |\n" +
-                "|   de segurança, além de registrar o incidente conforme os           |\n" +
-                "|   procedimentos internos.                                           |\n" +
-                "| - Realizar uma análise do incidente para identificar suas causas e  |\n" +
-                "|   implementar ações corretivas para prevenir ocorrências semelhantes.|\n" +
-                "|---------------------------------------------------------------------|\n" +
-                "|                                                                     |\n" +
-                "| 9. NORMAS E REGULAMENTOS APLICÁVEIS:                                |\n" +
-                "|                                                                     |\n" +
-                "| - Seguir todas as normas técnicas nacionais e internacionais relacionadas |\n" +
-                "|   à segurança e manutenção de equipamentos industriais.             |\n" +
-                "| - Cumprir as orientações do fabricante quanto ao uso e manutenção   |\n" +
-                "|   dos equipamentos.                                                 |\n" +
-                "|---------------------------------------------------------------------|\n";
+                "____________________________________________________________________________\n" +
+                "|-- OBJETIVO: Estabelecer os procedimentos operacionais padrão para      --|\n" +
+                "|    o setor de manutenção da WEG, garantindo a eficiência e a segurança   |\n" +
+                "|__________________________________________________________________________|\n" +
+                "|                                                                          |\n" +
+                "| 1. OBJETIVO DO SETOR DE MANUTENÇÃO:                                      |\n" +
+                "|                                                                          |\n" +
+                "| - Garantir a operação contínua e segura dos equipamentos e sistemas      |\n" +
+                "|   da WEG.                                                                |\n" +
+                "| - Realizar manutenção preventiva, corretiva e preditiva de acordo com    |\n" +
+                "|   os planos e cronogramas estabelecidos.                                 |\n" +
+                "|--------------------------------------------------------------------------|\n" +
+                "|                                                                          |\n" +
+                "| 2. MANUTENÇÃO PREVENTIVA:                                                |\n" +
+                "|                                                                          |\n" +
+                "| - Realizar inspeções regulares em todos os equipamentos e sistemas.      |\n" +
+                "| - Seguir os cronogramas de manutenção preventiva, conforme o manual      |\n" +
+                "|   do fabricante.                                                         |\n" +
+                "| - Lubrificação dos componentes conforme especificado no manual de        |\n" +
+                "|   operação e manutenção.                                                 |\n" +
+                "| - Verificar e substituir filtros, correias, rolamentos, etc., conforme   |\n" +
+                "|   necessário, para evitar falhas futuras.                                |\n" +
+                "|--------------------------------------------------------------------------|\n" +
+                "|                                                                          |\n" +
+                "| 3. MANUTENÇÃO CORRETIVA:                                                 |\n" +
+                "|                                                                          |\n" +
+                "| - Quando ocorrer falha ou defeito em algum equipamento, o setor de       |\n" +
+                "|   manutenção deverá realizar a análise do problema, elaborar um          |\n" +
+                "|   plano de reparo e corrigir a falha com a maior rapidez possível.       |\n" +
+                "| - Após a correção, realizar testes para garantir o funcionamento         |\n" +
+                "|   correto do equipamento.                                                |\n" +
+                "| - Documentar o defeito e o procedimento de reparo, para futuras          |\n" +
+                "|   referências e controle.                                                |\n" +
+                "|--------------------------------------------------------------------------|\n" +
+                "|                                                                          |\n" +
+                "| 4. MANUTENÇÃO PREDITIVA:                                                 |\n" +
+                "|                                                                          |\n" +
+                "| - Utilizar técnicas de monitoramento de condição (como termografia,      |\n" +
+                "|   vibração e ultrassom) para identificar potenciais falhas antes         |\n" +
+                "|   que ocorram.                                                           |\n" +
+                "| - Implementar tecnologias de análise preditiva para otimizar as          |\n" +
+                "|   intervenções de manutenção e reduzir os tempos de inatividade.         |\n" +
+                "| - Manter registros detalhados dos dados coletados para análise e         |\n" +
+                "|   previsão de falhas futuras.                                            |\n" +
+                "|--------------------------------------------------------------------------|\n" +
+                "|                                                                          |\n" +
+                "| 5. PROCEDIMENTOS DE SEGURANÇA NA MANUTENÇÃO:                             |\n" +
+                "|                                                                          |\n" +
+                "| - Desligar e isolar as fontes de energia antes de realizar qualquer      |\n" +
+                "|   intervenção em equipamentos.                                           |\n" +
+                "| - Utilizar Equipamentos de Proteção Individual (EPIs) adequados para     |\n" +
+                "|   cada tipo de tarefa, como luvas, óculos, capacetes, protetores         |\n" +
+                "|   auditivos e calçados de segurança.                                     |\n" +
+                "| - Assegurar que todas as ferramentas e dispositivos utilizados estejam   |\n" +
+                "|   em boas condições de uso e seguiam as normas de segurança.             |\n" +
+                "| - Em caso de necessidade de intervenções em áreas de risco, seguir       |\n" +
+                "|   as normas específicas de segurança para o trabalho em altura, em       |\n" +
+                "|   espaços confinados ou com risco elétrico.                              |\n" +
+                "|--------------------------------------------------------------------------|\n" +
+                "|                                                                          |\n" +
+                "| 6. DOCUMENTAÇÃO E REGISTROS DE MANUTENÇÃO:                               |\n" +
+                "|                                                                          |\n" +
+                "| - Manter um registro atualizado de todas as atividades de manutenção,    |\n" +
+                "|   incluindo a data, descrição do serviço realizado e peças trocadas.     |\n" +
+                "| - Os registros devem ser armazenados de forma organizada e de fácil      |\n" +
+                "|   acesso, para futuras auditorias ou referência.                         |\n" +
+                "| - Elaborar relatórios de falhas, identificando a causa raiz e as         |\n" +
+                "|   ações corretivas aplicadas.                                            |\n" +
+                "|--------------------------------------------------------------------------|\n" +
+                "|                                                                          |\n" +
+                "| 7. TREINAMENTO E CAPACITAÇÃO:                                            |\n" +
+                "|                                                                          |\n" +
+                "| - Todos os colaboradores do setor de manutenção devem participar de      |\n" +
+                "|   treinamentos periódicos sobre novas tecnologias, procedimentos e       |\n" +
+                "|   normas de segurança.                                                   |\n" +
+                "| - A WEG oferece programas de treinamento contínuo para garantir que      |\n" +
+                "|   os colaboradores estejam sempre atualizados com as melhores            |\n" +
+                "|   práticas de manutenção e segurança.                                    |\n" +
+                "|--------------------------------------------------------------------------|\n" +
+                "|                                                                          |\n" +
+                "| 8. PROCEDIMENTOS EM CASO DE INCIDENTES OU ACIDENTES:                     |\n" +
+                "|                                                                          |\n" +
+                "| - Caso ocorra qualquer tipo de acidente durante a manutenção, o          |\n" +
+                "|   colaborador deverá imediatamente acionar o supervisor e a equipe       |\n" +
+                "|   de segurança, além de registrar o incidente conforme os                |\n" +
+                "|   procedimentos internos.                                                |\n" +
+                "| - Realizar uma análise do incidente para identificar suas causas e       |\n" +
+                "|   implementar ações corretivas para prevenir ocorrências semelhantes.    |\n" +
+                "|--------------------------------------------------------------------------|\n" +
+                "|                                                                          |\n" +
+                "| 9. NORMAS E REGULAMENTOS APLICÁVEIS:                                     |\n" +
+                "|                                                                          |\n" +
+                "| - Seguir todas as normas técnicas nacionais e internacionais relacionadas|\n" +
+                "|   à segurança e manutenção de equipamentos industriais.                  |\n" +
+                "| - Cumprir as orientações do fabricante quanto ao uso e manutenção        |\n" +
+                "|   dos equipamentos.                                                      |\n" +
+                "|__________________________________________________________________________|\n";
 
 
         //manuais em INGLES
