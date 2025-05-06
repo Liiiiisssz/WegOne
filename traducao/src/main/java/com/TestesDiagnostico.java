6297c9b3e30989e4d0b82e73e64b1c65f28c5aac
Viihh-77 <gabrielli_glowatski@estudante.sesisenai.org.br>
2025-05-05 21:12:22 -0300

    Feat: adicionando lista de codigos excluirPT

--- traducao/src/main/java/com/TestesDiagnostico.java
+++ traducao/src/main/java/com/TestesDiagnostico.java
@@ -769,6 +769,16 @@ public class TestesDiagnostico {
 
     public void excluirManual() { //excluir PT
 
+		System.out.println("--LISTA DE CODIGOS CADASTRADOS--");
+		for (int i = 0; i < titulosTestesDiagnostico.length; i++) {
+
+			if (testeDiagnostico[i] != null) {
+				System.out.println(i);
+			}
+		}
+
+		System.out.println(" ");
+
 		System.out.println(tradutor.get("excluir"));
         int codigo = leia.nextInt();
 
