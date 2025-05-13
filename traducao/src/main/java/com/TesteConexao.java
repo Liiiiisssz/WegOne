5e200ea05fd49b129e048524bcf146db47331f57
Liiiiisssz <elis_jasper@estudante.sesisenai.org.br>
2025-05-13 20:20:37 -0300

    Test: conexao com o DB

--- traducao/src/main/java/com/TesteConexao.java
+++ traducao/src/main/java/com/TesteConexao.java
@@ -1,15 +1,17 @@
 package com;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public class TesteConexao {
-    public static void main(String[] args) {
-        Connection conn = Conexao.getConexao();
-
-        if (conn != null) {
-            System.out.println("Conexão estabelecida com sucesso!");
-        } else {
-            System.out.println("Falha na conexão.");
-        }   
+    public static void main(String[] args) throws SQLException {
+        try (Connection conn = Conexao.getConexao()) {
+            if (conn != null) {
+                System.out.println("Conexão estabelecida com sucesso!");
+            }
+        } catch (RuntimeException e) {
+            System.out.println("Erro ao conectar ao banco de dados: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
